e10c074d9e4c1f3af35920295cb548a88d9112af
Gayi Komi Selassi <gayi.int@ccdcindia.org>
2026-04-20 21:37:23 +0530

    remove language dependency in participant report

--- app.R
+++ app.R
@@ -650,7 +650,7 @@ server <- function(input, output, session) {
         sleep_data = sleep_path,
         participant_folder = part_dir,
         participant_files = list(
-          info = file.path(part_dir, "participants_info.xlsx"),
+          info = file.path(part_dir, "participants_info.xlsx")
           #ta = file.path(part_dir, "participants_ta.xlsx"),
           #hi = file.path(part_dir, "participants_hi.xlsx")
         ),
